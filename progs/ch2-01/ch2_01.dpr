e5e0db37c89ab230f74ca2acdc7ce62cf5618202
BertVerhees <bert.verhees@rosa.nl>
2014-03-10 07:53:12 +0100

    all projects

---
+++ progs/ch2-01/ch2_01.dpr
@@ -0,0 +1,24 @@
+program ch2_01;
+
+uses
+  {$IFDEF FPC}
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms,
+  {$ELSE}
+  Vcl.Forms,
+  {$ENDIF}
+  frmTest in 'frmTest.pas' {Form1},
+  expr in 'expr.pas',
+  uStreamLexer in 'uStreamLexer.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ progs/ch2-02/ch2_02.dpr
@@ -0,0 +1,25 @@
+program ch2_02;
+
+uses
+  {$IFDEF FPC}
+  {$IFDEF UNIX}
+  {$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF }
+  {$ENDIF }
+  Interfaces,
+  {$ELSE}
+  Vcl.Forms,
+  {$ENDIF }
+  frmTest in 'frmTest.pas' {Form1},
+  expr in 'expr.pas',
+  uStreamLexer in 'uStreamLexer.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
